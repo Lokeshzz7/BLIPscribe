b503344135bb3b707dcd2c028e834a80585e32ba
Lokeshwaran R <waranlokesh688@gmail.com>
2025-09-09 13:02:48 +0530

    implemented simple web interface using gradio and generated output with simple pretrained blip model

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ import gradio as gr
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
 
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base") 
+
+def caption_generation(img):
+    image = Image.fromarray(img)
+    inputs = processor(image , return_tensors = "pt")
+    outputs = model.generate(**inputs)
+    cap = processor.decode(outputs[0] , skip_special_tokens = True)
+    return cap
+
+web = gr.Interface(fn = caption_generation,inputs = [gr.Image(label = "Image")],outputs = [gr.Text(label = "Caption"),],)
+
+web.launch()
